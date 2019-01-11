9764a0b306feae16d0424f13903e070347ba8c69
Antonio <toncas@windowslive.com>
2019-01-11 15:29:51 -0800

    committing so that I can merge with upstream

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,11 @@
+const { Prisma } = require('prisma-binding');
+
+// connect to the remote prisma db
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,12 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const checkServerRun = serverDetails => console.log(`Server running on http://localhost:${serverDetails.port}`);
+const server = createServer();
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, checkServerRun)
